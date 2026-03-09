72157f60880a6e89d6fe86e1d698a4ca0bf882dc
joshua-rigg <joshua.rigg@stu.ocsb.ca>
2026-03-09 07:18:57 -0400

    Changed comments

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Joshua
+ * Created on: March 2026
+ * This program do the cookies clicker game 
 */
 
-basic.showString('Hello, World!')
+// variables
+let cookies: number = 0
+
+// start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// add cookies 
+input.onButtonPressed(Button.A, function () {
+    cookies = cookies + 1
+    basic.showNumber(cookies)
+})
+
+// stop
+input.onButtonPressed(Button.B, function () {
+    cookies = 0
+    basic.showNumber(cookies)
+})
+
